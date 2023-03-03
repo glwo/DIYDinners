66b361b101832b128d8986048f15392e9220331b
glen <glenwojnar@yahoo.com>
2023-03-03 12:12:57 -0500

    more error handling

--- react-app/src/components/CreateRecipe/index.js
+++ react-app/src/components/CreateRecipe/index.js
@@ -37,8 +37,8 @@ export default function CreateRecipe() {
       return;
     }
 
-    if (recipe_name.split(" ").length > 10) {
-      setErrors(["Recipe name cannot exceed 10 words."]);
+    if (recipe_name.split(" ").length > 6) {
+      setErrors(["Recipe name cannot exceed 6 words."]);
       return;
     }
 
@@ -52,8 +52,13 @@ export default function CreateRecipe() {
       return;
     }
 
+    if (ingredients.split(" ").length === 1 && ingredients.length > 25){
+      setErrors(["Recipe ingredients cannot exceed 25 characters"]);
+      return;
+    }
+
     if (ingredients.split(" ").length > 500){
-      setErrors(["Recipe description cannot exceed 500 words"]);
+      setErrors(["Recipe ingredients cannot exceed 500 words"]);
       return;
     }
 
